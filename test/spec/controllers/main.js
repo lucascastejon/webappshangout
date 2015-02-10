5661e566a3a1d4883794c85d6e90fa9bd9b9d0c9
Lucas Castejon <lucascastejon@gmail.com>
2015-02-10 01:41:37 -0200

    Altering the name module

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('webappshangoutApp'));
+  beforeEach(module('hangoutApp'));
 
   var MainCtrl,
     scope;
