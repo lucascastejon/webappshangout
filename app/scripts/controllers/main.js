d177a263d66dea91890a1a3ccca85bd888bcb3c4
Lucas Castejon <lucascastejon@gmail.com>
2015-02-09 23:16:55 -0200

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webappshangoutApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webappshangoutApp
+ */
+angular.module('webappshangoutApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
